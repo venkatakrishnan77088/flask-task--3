3496d5cbec86acd21256fabcb73a35f46a863bcf
venkatakrishnan77088 <vkproplayer1234@gmail.com>
2023-01-11 15:11:39 +0530

    adding task 3

---
+++ app.py
@@ -0,0 +1,79 @@
+from flask import Flask, render_template,request
+import requests
+
+
+app = Flask(__name__)
+
+
+dic={
+    "page": 2,    
+    "per_page": 6,
+    "total": 12,
+    "total_pages": 2,
+    "data": [
+        {
+            "id": 7,
+            "email": "michael.lawson@reqres.in",
+            "first_name": "Michael",
+            "last_name": "Lawson",
+            "avatar": "https://reqres.in/img/faces/7-image.jpg"
+        },
+        {
+            "id": 8,
+            "email": "lindsay.ferguson@reqres.in",
+            "first_name": "Lindsay",
+            "last_name": "Ferguson",
+            "avatar": "https://reqres.in/img/faces/8-image.jpg"
+        },
+        {
+            "id": 9,
+            "email": "tobias.funke@reqres.in",
+            "first_name": "Tobias",
+            "last_name": "Funke",
+            "avatar": "https://reqres.in/img/faces/9-image.jpg"
+        },
+        {
+            "id": 10,
+            "email": "byron.fields@reqres.in",
+            "first_name": "Byron",
+            "last_name": "Fields",
+            "avatar": "https://reqres.in/img/faces/10-image.jpg"
+        },
+        {
+            "id": 11,
+            "email": "george.edwards@reqres.in",
+            "first_name": "George",
+            "last_name": "Edwards",
+            "avatar": "https://reqres.in/img/faces/11-image.jpg"
+        },
+        {
+            "id": 12,
+            "email": "rachel.howell@reqres.in",
+            "first_name": "Rachel",
+            "last_name": "Howell",
+            "avatar": "https://reqres.in/img/faces/12-image.jpg"
+        }
+    ],
+    "support": {
+        "url": "https://reqres.in/#support-heading",
+        "text": "To keep ReqRes free, contributions towards server costs are appreciated!"
+    }
+}
+ 
+@app.route("/")
+def api():
+    d=dic.get("data")
+    return render_template("index.html",data=d)
+
+
+@app.route("/api",methods=["post","get"])
+def apiid():
+    
+    for i in dic.get("data"):
+        if i.get("id")==int(request.json.get("id")):
+            return i
+        
+
+
+if __name__=="__main__":
+    app.run(debug=True)
